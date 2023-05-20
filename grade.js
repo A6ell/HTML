bda64a4a71901a419122913102405456ab22883d
a6ell <a6ell.demere@gmail.com>
2023-05-21 00:56:06 +0300

    Grade

--- grade.js
+++ grade.js
@@ -1,27 +1,48 @@
-function getToday() {
-    let Grade;
-    switch (Grade){
-        case 0:
-            Grade = "sunGrade";
-            break;
-        case 1:
-            Grade = "MonGrade"
-            break;
-        case 2:
-            Grade = "TuesGrade"
-            break;
-        case 3:
-            Grade = "WednesGrade"
-            break;
-        case 4:
-            Grade = "ThursGrade"
-            break;
-        case 5:
-            Grade = "FriGrade"
-            break;
-        case 6:
-            Grade = "SaturGrade"
-            break;
+function calculateGrade(mark) {
+    var grade;
+  
+    switch (true) {
+      case mark >= 95:
+        grade = 'A';
+        break;
+      case mark >= 90:
+        grade = 'A-';
+        break;
+      case mark >= 85:
+        grade = 'B+';
+        break;
+      case mark >= 80:
+        grade = 'B';
+        break;
+      case mark >= 75:
+        grade = 'B-';
+        break;
+      case mark >= 70:
+        grade = 'C+';
+        break;
+      case mark >= 65:
+        grade = 'C';
+        break;
+      case mark >= 60:
+        grade = 'C-';
+        break;
+      case mark >= 55:
+        grade = 'D+';
+        break;
+      case mark >= 50:
+        grade = 'D';
+        break;
+      default:
+        grade = 'F';
     }
-    document.getElementById("demo").innerHTML = "Today is " + Grade;
-}
+  
+    return grade;
+  }
+  
+  function DisplayGrade() {
+    var name = document.getElementById("nameInput").value;
+    var mark = parseFloat(document.getElementById("markInput").value);
+    var grade = calculateGrade(mark);
+    document.getElementById("demo").innerText = "Name: " + name + "\nMark: " + mark + "\nGrade: " + grade;
+  }
+  
